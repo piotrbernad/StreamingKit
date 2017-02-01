2c6bf17c4d8641ec19e7bb0595b2cea2b6f07ba1
Piotr Bernad <piotr.bernad@appunite.com>
2017-02-01 09:42:48 +0100

    Carthage Support Added

---
+++ StreamingKit/StreamingKit.h
@@ -0,0 +1,19 @@
+//
+//  StreamingKit.h
+//  StreamingKit
+//
+//  Created by Piotr Bernad on 01.02.2017.
+//  Copyright © 2017 Piotr Bernad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StreamingKit.
+FOUNDATION_EXPORT double StreamingKitVersionNumber;
+
+//! Project version string for StreamingKit.
+FOUNDATION_EXPORT const unsigned char StreamingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamingKit/PublicHeader.h>
+
+
